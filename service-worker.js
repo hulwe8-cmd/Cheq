253e8a8633cc4bf3a400ff5992633b8125460b78
hulwe8-cmd <hulwe8@gmail.com>
2026-05-28 13:24:56 -0400

    Update service worker cache and file paths

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,43 @@
-const CACHE_NAME = "cheq-local-v6-ledger-ui-may";
+const CACHE_NAME = "cheq-cache-v10";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icons/logo.svg",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/Cheq/",
+  "/Cheq/index.html",
+  "/Cheq/style.css",
+  "/Cheq/script.js",
+  "/Cheq/manifest.json",
+  "/Cheq/icons/icon-192.png",
+  "/Cheq/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames.map(cacheName => {
-        if (cacheName !== CACHE_NAME) {
-          return caches.delete(cacheName);
-        }
-        return null;
-      })
-    ))
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") {
-    return;
-  }
-
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
